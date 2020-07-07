bc128d69bfbe531775a2dd1b8ec518b5e4ee3963
neodevelop <juan@makingdevs.com>
2020-07-06 20:06:54 -0500

    Ciclo de vida de Spock con métodos y groovy truth

--- src/test/groovy/com/makingdevs/FullSpec.groovy
+++ src/test/groovy/com/makingdevs/FullSpec.groovy
@@ -4,7 +4,16 @@ import spock.lang.*
 
 class FullSpec extends Specification {
 
-  @Unroll("cuando #cadena se convierte a mayusculas es #cadena_mayus y tiene longitud #tamanio")
+  def setup() { println "Setup" } // Antes de todo cada specs definidos aquí
+  def cleanup() { println "Cleanup" } // Después de cada spec definidos
+  def setupSpec() { println "setupSpec"} // Antes de todos los Spec definidos
+  def cleanupSpec() { println "cleanupSpec"} // Después de todos los Spec definidos
+
+  @Unroll("""\
+    cuando #cadena se convierte a mayusculas
+    es #cadena_mayus
+    y tiene longitud #tamanio
+  """)
   def "mostrar el ciclo de ejecución de Spock"() {
     given: "Dado un elemento de inicio"
     // setup: "Inicializa los reecursos para la especificación"
@@ -26,6 +35,18 @@ class FullSpec extends Specification {
     "hola"  | "HOLA"             | 4
     "mundo" | "MUNDO"            | 5
     "foo"   | "FOO"              | 3
+  }
+
+  def "Dadod un estímulo al systema/clase se espera una respuesta"() {
+    expect:
+      [1]
+      [uno: 1]
+      "No empty"
+      -1
+      1
+      true
+      1.3
+      new Object()
   }
 }
 
