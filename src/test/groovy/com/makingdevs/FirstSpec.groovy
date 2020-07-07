d44d129a83b638ef18aa38843e9dbea75648eb67
neodevelop <juan@makingdevs.com>
2020-07-06 19:24:40 -0500

    Agrego mi primer Spec

---
+++ src/test/groovy/com/makingdevs/FirstSpec.groovy
@@ -0,0 +1,20 @@
+package com.makingdevs
+
+import spock.lang.*
+
+class FirstSpec extends Specification {
+
+  def "Foo first specification"(){
+    expect:
+      1 == 1
+  }
+
+  def "Agregar un nuevo objeto a un Set"(){
+    given:
+      def bag = [2,3,4,5] as Set
+    when:
+      bag << 1
+    then:
+      bag.size() == 5
+  }
+}
