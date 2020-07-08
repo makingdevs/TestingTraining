20f7a2d0b7d67178a2678631c95e0045daa630b0
neodevelop <juan@makingdevs.com>
2020-07-08 18:47:39 -0500

    Agrego dependencias que usa el

--- src/main/java/com/makingdevs/model/Transaction.java
+++ src/main/java/com/makingdevs/model/Transaction.java
@@ -1,4 +1,15 @@
 package com.makingdevs.model;
 
+import java.math.BigDecimal;
+
 public class Transaction {
+  private BigDecimal amount;
+
+  public void setAmount(BigDecimal amount){
+    this.amount = amount;
+  }
+
+  public BigDecimal getAmount(){
+    return this.amount;
+  }
 }
--- src/main/java/com/makingdevs/services/TransactionService.java
+++ src/main/java/com/makingdevs/services/TransactionService.java
@@ -2,8 +2,20 @@ package com.makingdevs.services;
 
 import java.util.List;
 import java.math.BigDecimal;
+import com.makingdevs.model.Transaction;
+import com.makingdevs.services.PaymentGateway;
 
 public class TransactionService {
+
+  private PaymentGateway paymentGateway;
+
+  public void setPaymentGateway(PaymentGateway pg){
+    this.paymentGateway = pg;
+  }
+
+  public PaymentGateway getPaymentGateway(){
+    return this.paymentGateway;
+  }
 
   public int countRecordsFor(String table){
     if(table.equals("") || table == null)
@@ -20,6 +32,11 @@ public class TransactionService {
   }
 
   public int doPayment(BigDecimal amount){
+
+    Transaction trx = new Transaction();
+    trx.setAmount(amount);
+    boolean passed = paymentGateway.authorize(trx);
+    if(passed) return 304;
     return  0;
   }
 
