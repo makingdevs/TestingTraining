6cdfd4e7a7fdcbd73511f099f1498cbb39fffbe0
neodevelop <juan@makingdevs.com>
2020-07-11 09:11:28 -0500

    Agrego un path para el recurso

--- src/main/java/com/makingdevs/repository/StringCalculatorRepository.java
+++ src/main/java/com/makingdevs/repository/StringCalculatorRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import com.makingdevs.model.StringCalculatorData;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "string_calculator")
 public interface StringCalculatorRepository extends PagingAndSortingRepository<StringCalculatorData, Long> {
 }
