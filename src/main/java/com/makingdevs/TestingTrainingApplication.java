427169ac0635305e9edf2741d5b52739a991d60c
neodevelop <juan@makingdevs.com>
2020-07-10 20:24:56 -0500

    Ajusto la clase de Boot

--- src/main/java/com/makingdevs/TestingTrainingApplication.java
+++ src/main/java/com/makingdevs/TestingTrainingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingTrainingApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(AppApplication.class, args);
+    SpringApplication.run(TestingTrainingApplication.class, args);
   }
 
 }
