993b971bb3604f4cf0a0fa6158985ac0bb63a5d3
neodevelop <juan@makingdevs.com>
2020-07-10 20:23:42 -0500

    Agrego elementos principales de Spring

---
+++ src/main/java/com/makingdevs/TestingTrainingApplication.java
@@ -0,0 +1,14 @@
+package com.makingdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingTrainingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppApplication.class, args);
+  }
+
+}
+
