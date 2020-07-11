3c8678c6fbe145a02edc3d8fdfa057c00719ef09
neodevelop <juan@makingdevs.com>
2020-07-10 21:02:38 -0500

    Test si existe el Datasource

--- src/test/groovy/com/makingdevs/TestingTrainingApplicationIntegrationSpec.groovy
+++ src/test/groovy/com/makingdevs/TestingTrainingApplicationIntegrationSpec.groovy
@@ -4,6 +4,7 @@ import spock.lang.*
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
+import javax.sql.DataSource
 
 @SpringBootTest
 class TestingTrainingApplicationIntegrationSpec extends Specification {
@@ -11,9 +12,13 @@ class TestingTrainingApplicationIntegrationSpec extends Specification {
   @Autowired
   ApplicationContext applicationContext
 
+  @Autowired
+  DataSource dataSource
+
   def "when startup the application context is up"() {
     expect:
       applicationContext
+      dataSource
   }
 
 }
