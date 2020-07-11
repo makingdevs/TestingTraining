0e5c2a65af331948a06c3252685cf67b881def11
neodevelop <juan@makingdevs.com>
2020-07-11 09:00:56 -0500

    Agrego un nuevo spec para el repo

--- src/test/groovy/com/makingdevs/TransactionIntegrationSpec.groovy
+++ src/test/groovy/com/makingdevs/TransactionIntegrationSpec.groovy
@@ -6,6 +6,8 @@ import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.transaction.annotation.Propagation
+import com.makingdevs.repository.StringCalculatorRepository
+import com.makingdevs.model.StringCalculatorData
 
 @SpringBootTest
 @Transactional
@@ -13,6 +15,9 @@ class TransactionIntegrationSpec extends Specification {
 
   @Autowired
   JpaTransactionManager jpaTransactionManager
+
+  @Autowired
+  StringCalculatorRepository repository
 
   //@Transactional(propagation = Propagation.REQUIRES_NEW)
   def "do a transaction"() {
@@ -24,6 +29,15 @@ class TransactionIntegrationSpec extends Specification {
       println "*"*100
     then:
       transaction
+  }
+
+  def "create a new string calculator data"() {
+    given:
+      def data = new StringCalculatorData(input:"", result: 0)
+    when:
+      def result = repository.save(data)
+    then:
+      result.id
   }
 
 }
